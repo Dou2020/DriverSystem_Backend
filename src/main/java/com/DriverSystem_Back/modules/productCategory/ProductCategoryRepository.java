6604e2ae1e48759262064480bf0e00094515f56e
Dou <doueduar2018@gmail.com>
2025-08-20 23:44:35 -0600

    update branch

---
+++ src/main/java/com/DriverSystem_Back/modules/productCategory/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.DriverSystem_Back.modules.productCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+    Optional<ProductCategory> findByName(String name);
+}
